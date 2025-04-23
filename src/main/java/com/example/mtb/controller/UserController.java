4306dba375a4197453e5b82a7e4807d753b94a58
naveenpr-728 <naveenkumar.pr28@gmail.com>
2025-04-23 15:38:25 +0530

    updating resolve

--- src/main/java/com/example/mtb/controller/UserController.java
+++ src/main/java/com/example/mtb/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.mtb.controller;
 
 import com.example.mtb.dto.UserRegistrationRequest;
 import com.example.mtb.dto.UserResponse;
+import com.example.mtb.dto.UserUpdationRequest;
 import com.example.mtb.entity.UserDetails;
 import com.example.mtb.service.UserService;
 import com.example.mtb.utility.ResponseStructure;
@@ -9,9 +10,7 @@ import com.example.mtb.utility.RestResponseBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -25,7 +24,12 @@ public class UserController {
     public ResponseEntity<ResponseStructure<UserResponse>> addUser(@RequestBody UserRegistrationRequest user) {
         UserResponse userDetails = userservice.addUser(user);
         return ResponseBuilder.sucess(HttpStatus.OK, "New user details added ", userDetails);
-
-
     }
-}
+
+        @PutMapping("/users/{email}")
+        public ResponseEntity<ResponseStructure<UserResponse>> editUser(@PathVariable String email, @RequestBody UserUpdationRequest user){
+            UserResponse userDetails = userservice.editUser(user, email);
+            return ResponseBuilder.sucess(HttpStatus.OK,"User Details has been updated", userDetails);
+        }
+    }
+
--- src/main/java/com/example/mtb/service/UserService.java
+++ src/main/java/com/example/mtb/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.mtb.service;
 
 import com.example.mtb.dto.UserRegistrationRequest;
 import com.example.mtb.dto.UserResponse;
+import com.example.mtb.dto.UserUpdationRequest;
 import com.example.mtb.entity.UserDetails;
 
 public interface UserService {
@@ -9,4 +10,5 @@ public interface UserService {
    // public userDetails addUser(userDetails user);
    // public UserDetails addUser(UserRegistrationRequest user);
     public UserResponse addUser(UserRegistrationRequest user);
+    UserResponse editUser(UserUpdationRequest user, String email);
 }
