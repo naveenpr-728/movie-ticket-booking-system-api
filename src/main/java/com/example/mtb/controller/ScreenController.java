2ffd36ba5e6c8a3cfffaebcc9e82f6a45065e51c
naveenpr-728 <naveenkumar.pr28@gmail.com>
2025-04-28 20:23:10 +0530

    screen and seats insertion

---
+++ src/main/java/com/example/mtb/controller/ScreenController.java
@@ -0,0 +1,28 @@
+package com.example.mtb.controller;
+
+import com.example.mtb.dto.ScreenRequest;
+import com.example.mtb.dto.ScreenResponse;
+import com.example.mtb.service.ScreenService;
+import com.example.mtb.utility.ResponseStructure;
+import com.example.mtb.utility.RestResponseBuilder;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class ScreenController {
+
+    private final ScreenService screenService;
+    private final RestResponseBuilder responseBuilder;
+
+    @PostMapping("theaters/{theaterId}/screens")
+    public ResponseEntity<ResponseStructure<ScreenResponse>> addScreen(@RequestBody ScreenRequest screenRequest, @PathVariable String theaterId){
+        ScreenResponse screenResponse = screenService.addScreen(screenRequest, theaterId);
+        return responseBuilder.sucess(HttpStatus.OK, "Screen has been successfully created", screenResponse);
+    }
+}
---
+++ src/main/java/com/example/mtb/enums/ScreenType.java
@@ -0,0 +1,8 @@
+package com.example.mtb.enums;
+
+public enum ScreenType {
+
+    IMAX,
+    TWO_D,
+    THREE_D
+}
